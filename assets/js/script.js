702931b77ba29243e44cda3110f30ef07c31fa3c
NuclearReid <reid.backcnmt@gmail.com>
2024-01-07 13:39:35 +1100

    gets values when the user selects a movie genre/classification

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,84 @@
 // API key and URLs
 
-var api_key = "9c8e80a7";
-var base_url = "http://www.omdbapi.com/?i=tt3896198&apikey=";
+var api_key = "&apikey=9c8e80a7";
+var base_url = "http://www.omdbapi.com/?t=";
 var movie_id = ""
+// var movieName = prompt('enter in a movie name');
+// var totalUrl = base_url + movieName + api_key;
 
-function getMovieDetails(movie_id) {
-    var url = `${base_url}${api_key}/${movie_id}`;
+var chosenMovie;
+var correctClass = false;
+var theClass;
+function handleFormSubmit(event){
+    event.preventDefault();
+    correctClass = false;
+    var genre = $('#genre').val();
+    var rating = $('#rating').val();
+    var randomGenre = -1;
+    console.log(genre + ' ' + rating);
+    do{
+        if(genre == 'any-genre'){
+            randomGenre = Math.floor(Math.random()*4);
+            console.log(randomGenre);
+        }
+        if(genre == 'action' || randomGenre == 0){
+            var actionMoviePick = Math.floor(Math.random()*adventureMovies.length);
+            chosenMovie = adventureMovies[actionMoviePick][0];
+            theClass = adventureMovies[actionMoviePick][1];
+            if(theClass == rating || rating == 'any-classification'){
+                correctClass = true;
+                console.log(chosenMovie +' and the classification '+ theClass);
+            }
+            // console.log(chosenMovie +' and the classification '+ theClass);
+        }
+        if(genre == 'comedy'|| randomGenre == 1){
+            var comedyMoviePick = Math.floor(Math.random()*comedyMovies.length);
+            chosenMovie = comedyMovies[comedyMoviePick][0];
+            theClass = comedyMovies[comedyMoviePick][1];
+            if(theClass == rating || rating == 'any-classification'){
+                correctClass = true;
+                console.log(chosenMovie +' and the classification '+ theClass);
+            }
+            // console.log(chosenMovie +' and the classification '+ theClass);
+        }
+        if(genre == 'drama'|| randomGenre == 2){
+            var dramaMoviePick = Math.floor(Math.random()*dramaMovies.length);
+            chosenMovie = dramaMovies[dramaMoviePick][0];
+            theClass = dramaMovies[dramaMoviePick][1];
+            if(theClass == rating || rating == 'any-classification'){
+                correctClass = true;
+                console.log(chosenMovie +' and the classification '+ theClass);
+            }
+            // console.log(chosenMovie +' and the classification '+ theClass);
+        }
+        if(genre == 'fantasy'|| randomGenre == 3){
+            var fantasyMoviePick = Math.floor(Math.random()*fantasyMovies.length);
+            chosenMovie = fantasyMovies[fantasyMoviePick][0];
+            theClass = fantasyMovies[fantasyMoviePick][1];
+            if(theClass == rating || rating == 'any-classification'){
+                correctClass = true;
+                console.log(chosenMovie +' and the classification '+ theClass);
+            }
+        }
+    }while(correctClass == false);
+}
+$('#selectionForm').on('submit', handleFormSubmit);
 
+
+function movieAPIcall(){
     $.ajax({
-        url: url,
+        url: totalUrl,
         type: 'GET',
-        dataType: 'json',
-        success: function(response) {
+        // dataType: 'json',
+    }).then(function(response){
             // Process and use response as needed
-            console.log(response);
-        },
-        error: function(xhr, status, error) {
-            console.error(`Error: ${xhr.status} - ${error}`);
+            console.log(response);   
+
+    }).fail(function(fail){
+        if(fail.status !== 200){
+            alert('Could not get the info for that city');
+            return;
         }
     });
 }
-
-getMovieDetails(movie_id);  // Replace with an actual IMDb movie ID
+// getMovieDetails(movie_id);  // Replace with an actual IMDb movie ID
