b0cf722aa048c06c3ad110aa57c231a296b9baf0
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-01-10 17:57:49 +1100

    hidden modal to address api issue

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,28 +1,27 @@
-function openModal() {
-    document.getElementById('modal-saved-movies').classList.add('is-active');
-}
+// function openModal() {
+//     document.getElementById('modal-saved-movies').classList.add('is-active');
+// }
 
-// Function to close the modal
-function closeModal() {
-    document.getElementById('modal-saved-movies').classList.remove('is-active');
-}
+// // Function to close the modal
+// function closeModal() {
+//     document.getElementById('modal-saved-movies').classList.remove('is-active');
+// }
 
-// When button is clicked modal closes
-document.getElementById('modal-saved-movies').addEventListener('click', function () {
-    closeModal();
-});
+// // When button is clicked modal closes
+// document.getElementById('modal-saved-movies').addEventListener('click', function () {
+//     closeModal();
+// });
 
-// Add event listener to the form select
-document.getElementById('saved-movies').addEventListener('change', function () {
-    // Check the selected option value
-    var selectedOption = this.value;
+// // Add event listener to the form select
+// document.getElementById('saved-movies').addEventListener('change', function () {
+//     // Check the selected option value
+//     var selectedOption = this.value;
 
-    // If a specific option is selected, open the modal
-    // not sure how to select anything and its generated?
-    if (selectedOption === selectedOption) {
-        openModal();
-    } else {
-        closeModal();
-    }
-});
-
+//     // If a specific option is selected, open the modal
+//     // not sure how to select anything and its generated?
+//     if (selectedOption === selectedOption) {
+//         openModal();
+//     } else {
+//         closeModal();
+//     }
+// });
